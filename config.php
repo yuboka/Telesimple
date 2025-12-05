06eb33ef6f8ec76586340b44f9751b061cb64d8f
yuboka <42755153+yuboka@users.noreply.github.com>
2025-12-06 00:06:18 +0100

    Update and rename Config.php to config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Telegram Bot Token
+define("BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN");
+define("TELEGRAM_API", "https://api.telegram.org/bot" . BOT_TOKEN . "/");
+
+// SABUS API KEY
+define("SABUS_KEY", "YOUR_SABUS_API_KEY");
+
+// SABUS API Base URL
+define("SABUS_BASE", "https://sabusapi.com/api/");
